93db7254789c49690c390c3862ec2a4821d79288
chicken <hibary.taiwan@gmail.com>
2022-10-29 22:32:02 +0800

    feat: generate jwt page

---
+++ MauiApp-Test/Data/JwtService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp_Test.Data
+{
+    public class JwtService
+    {
+        public Task<string> Get(string userName, string password)
+        {
+            return Task.FromResult("12314436");
+        }
+    }
+}
