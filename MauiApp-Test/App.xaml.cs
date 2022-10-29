1b488a7b7478676fe618b614efa4a8535e26b1ca
chicken <hibary.taiwan@gmail.com>
2022-10-29 21:58:09 +0800

    init

---
+++ MauiApp-Test/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp_Test;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiApp-Test/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp_Test;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
